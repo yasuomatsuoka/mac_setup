95ee6e03a1e4561714f47b294435fecf32d0ab39
yasuo.matsuoka <ysomtok@gmail.com>
2022-07-05 07:39:43 +0900

    update.

--- scripts/initial.sh
+++ scripts/initial.sh
@@ -6,7 +6,7 @@ sudo xcode-select --install
 
 ## install homebrew
 mkdir ~/.homebrew
-curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C .homebrew
+curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/.homebrew
 brew update
 brew install zsh
 brew install ansible
